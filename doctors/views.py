04dc423472549efe5783bf18291428fa09e2e699
zaebee <zaebuntu@gmail.com>
2017-10-15 02:03:27 +0300

    iadded ical import

--- doctors/views.py
+++ doctors/views.py
@@ -1,6 +1,9 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
+from datetime import datetime, timedelta
 from django.shortcuts import render
+
+from django_ical.views import ICalFeed
 
 from rest_framework import viewsets, filters
 from rest_framework.response import Response
@@ -33,3 +36,49 @@ class PatientViewSet(viewsets.ModelViewSet):
     """
     serializer_class = PatientSerializer
     queryset = Patient.objects.all()
+
+
+class EventFeed(ICalFeed):
+    """
+    A simple event calender
+    """
+    product_id = '-//Yandex LLC//Yandex Calendar//EN'
+    timezone = 'UTC'
+    file_name = "event.ics"
+
+    def items(self):
+        return Schedule.objects.all().order_by('-day_of_week')
+
+    def item_title(self, item):
+        return item.patient
+
+    def item_description(self, item):
+        return '%s. Пациент: %s (%s)' % (
+            item.doctor,
+            item.patient,
+            item.patient.info
+        )
+
+    def item_start_datetime(self, item):
+        date = item.day_of_week.strftime('%Y%m%d')
+        hour = item.get_hour_display().replace(':', '')
+        start = date + hour
+        start = datetime.strptime(start, '%Y%m%d%H%M')
+        return start
+
+    def item_end_datetime(self, item):
+        start = self.item_start_datetime(item)
+        end = start + timedelta(minutes=30)
+        return end
+
+    def item_link(self, item):
+        return '/schedules/%s' % item.id
+
+    def item_location(self, item):
+        return 'Атмосфера'
+
+    def item_categories(self, item):
+        return 'new year'
+
+    def item_transparency(self, item):
+        return 'OPAQUE'
--- entrydoc/urls.py
+++ entrydoc/urls.py
@@ -20,10 +20,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 from django.views.generic import TemplateView
+from doctors import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="base.html"))
+    url(r'^$', TemplateView.as_view(template_name="base.html")),
+    url(r'^feed.ics$', views.EventFeed()),
 ]
+
 
 urlpatterns += [
     url(r'^admin/', include(admin.site.urls)),
